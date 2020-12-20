56932caeb3174552f6d54ce5be85a8fd40009f69
taolx0 <taolx25@foxmail.com>
2020-12-20 14:12:37 +0800

    by normal way

--- Sort/insertionSort/main.go
+++ Sort/insertionSort/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func main() {
+	palindrome := isPalindrome(11211)
+	fmt.Println(palindrome)
+}
+
+func isPalindrome(x int) bool {
+	s := strconv.Itoa(x)
+	b := true
+	var i, j int
+	j = len(s) - 1
+	for i = 0; i < j; i++ {
+		if s[i] == s[j] {
+			b = true
+		} else {
+			b = false
+		}
+		j--
+	}
+	return b
 }
